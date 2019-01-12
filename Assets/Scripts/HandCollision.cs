772becde77a744e6615eb53da1b2c8607ed49d5e
jefflongo <jeff.a.longo@gmail.com>
2019-01-12 12:56:14 -0800

    Add hand collision detection, fix errors

---
+++ Assets/Scripts/HandCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("HandR")) {
+            Debug.Log("Right hand detected");
+        }
+    }
+}
